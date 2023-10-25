4363c879ad639dd6e21d31251f4b00a952db4706
yaniravenegas <ynra.venegas@gmail.com>
2023-10-24 23:23:33 -0300

    Se agrega archivo room7.js para actividad clase 5

---
+++ room7.js
@@ -0,0 +1,43 @@
+/* function test1 (x, y) {
+    return y - x
+}
+
+test1 (10, 40)
+console.log(test1(10, 40))
+*/
+
+/*function test2 (x, y) {
+    return x*2
+    console.log(x)
+    return x/2
+}
+
+console.log(test2(10))
+*/
+
+//----------------------------
+/*const prompt = require("prompt-sync")({ sigint: true });
+
+let peso = parseFloat(prompt("Ingrese su peso en kg: "));
+let altura = parseFloat(prompt("Ingrese su altura en metros: "));
+
+
+//function calculadorIMC (peso, altura) {
+//    return peso/(altura*altura)
+//}
+
+let calculadorIMC = (peso, altura) => peso/(altura*altura)
+
+const imc = calculadorIMC(peso, altura);
+console.log("Su Índice de Masa Corporal (IMC) es: " + imc);
+*/
+
+const prompt = require("prompt-sync")({ sigint: true });
+
+let input = prompt("Ingrese su string en minúsculas: ");
+
+function textoMayusculas (input) {
+    return input.toUpperCase();
+}
+
+console.log("Resultado: "+textoMayusculas(input))
